cabf4a1bcb923d867b8d0261b50316af310be615
IgnacioAlme <ignacio.yair.almenar@gmail.com>
2024-10-09 10:37:11 -0300

    Página simple para ver artículos

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
-  password: "",
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
   port: process.env.MYSQL_PORT,
   waitForConnections: true,
