18acdacf478272053dfa1a1f5f752552e4f5163c
Facundo Uferer <juanfacundouf@gmail.com>
2024-12-03 13:06:38 -0300

    Reemplazando contenido de main con el de facundev

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,35 +1,14 @@
-const mysql = require("mysql2/promise");
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
+  host: 'studio.cursos.scyt.gar.com.ar',
+  port: 1906,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
-  port: process.env.MYSQL_PORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
+  queueLimit: 0
 });
 
-async function query(sql, params) {
-  const [rows, fields] = await pool.execute(sql, params);
-  return rows;
-}
-
-async function testConnection() {
-  try {
-    const result = await query("SELECT 1");
-    console.log("Database connection successful");
-    console.log("Test query result:", result);
-    return true;
-  } catch (error) {
-    console.error("Database connection failed");
-    console.error(error);
-    return false;
-  }
-}
-
-module.exports = {
-  query,
-  testConnection,
-};
+export default pool; 
